e5f4053222325fc3b19f23159170503b10d77376
Leon Namowitz <leonnamowitzschool@gmail.com>
2022-12-03 14:43:00 +0100

    old version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+
+    private static LinkedList<Integer> bubbleSort(LinkedList<Integer> array) {    // time complexity: O(n^2) - worst case
+        int n = array.size();
+        for (int i = 0; i < n - 1; i++)    // all elements checked after each other
+            for (int j = 0; j < n - 1; j++)    // single element moving through array
+                if (array.get(j) > array.get(j + 1)) {
+                    // swap current with next element
+                    int temp = array.get(j);
+                    array.set(j, array.get(j + 1));
+                    array.set(j + 1, temp);
+                }
+        return array;
     }
 }
